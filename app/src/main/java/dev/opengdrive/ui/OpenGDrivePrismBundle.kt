8275abd1b2cacba5169ed4e6943917bfbfe90e77
jokerm4 <sundust@jokerm4deMac-mini-4478.local>
2026-08-11 10:05:29 +0800

    Fix Prism grammar bundle declaration

--- app/src/main/java/dev/opengdrive/ui/OpenGDrivePrismBundle.kt
+++ app/src/main/java/dev/opengdrive/ui/OpenGDrivePrismBundle.kt
@@ -3,7 +3,7 @@ package dev.opengdrive.ui
 import io.noties.prism4j.annotations.PrismBundle
 
 @PrismBundle(
-    includes = [
+    include = [
         "c", "cpp", "csharp", "css", "dart", "go", "groovy", "java", "javascript",
         "json", "kotlin", "latex", "makefile", "markdown", "markup", "python", "scala",
         "sql", "swift", "yaml",
